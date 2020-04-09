1861899b9c4abdddb6f59e2dd8e9cf91f8b941aa
wml <wml@e700.cn>
2020-04-09 19:58:27 +0800

    你好世界
    你好世界

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '你好，世界',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
